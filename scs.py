b193e61bc824d1fbcbadf1ea5e46dfa195a04351
Pablo Galindo Salgado <pablogsal@gmail.com>
2015-12-28 17:15:38 +0100

    Solved the scs QftG

--- scs.py
+++ scs.py
@@ -8,10 +8,107 @@ For example: ["suit","case","it"] -> "suitcase" (and also "casesuit")
              ["suit","case","it","suitcase"] -> "suitcase" (now unique)
 """
 
+import collections
+import heapq
+
+Partition = collections.namedtuple('Partition', 'prefix sufix')
+
+class Heap(object):
+    """A min-heap."""
+
+    def __init__(self):
+        self._values = []
+
+    def push(self, value):
+        """Push the value item onto the heap."""
+        heapq.heappush(self._values, value)
+
+    def pop(self):
+        """ Pop and return the smallest item from the heap."""
+        return heapq.heappop(self._values)
+
+    def __len__(self):
+        return len(self._values)
+
+    def __iter__(self):
+        yield from self._values
+
+class ProtoSuperstring(collections.defaultdict):
+
+    def __init__(self,*args,**kwargs):
+
+        self._keys = Heap()
+        super().__init__(*args,**kwargs)
+
+    def __setitem__(self, key, value):
+        if key not in self._keys:
+            self._keys.push(key)
+
+        super().__setitem__(key,value)
+
+    def popmin(self):
+
+        return self._keys.pop()
+
+
+def generate_partitions(word):
+
+    for i in reversed(range(len(word)+1)):
+        yield Partition(prefix=word[:i],sufix=word[i:])
+
 
 def minSuperstring(wordList):
-    # TODO: Write me!
-    pass
+    """
+    Generate the shortest superstring that contains all the given substrings
+    :param wordList: List of all the substrings - List of str
+    :return: String representing the superstring - Str
+    """
+
+    # We will use Dykstra algorithm to do this thing. The strategy is to glue the substrings together until we have no
+    # more substrings to glue. The problem is that a given substring could be in our proto-superstring so when we
+    # "glue" substring together we have to "trim" the common parts.
+
+    proto_superstrings = ProtoSuperstring(set)
+
+    max_len = len("".join(wordList))
+
+    # Start with the empty string
+
+    proto_superstrings[0].add("")
+
+    while True:
+
+        # Iterate thought the  minimum set of superstrings
+
+        min_len = proto_superstrings.popmin()
+
+        while proto_superstrings[min_len]:
+
+            prefix = proto_superstrings[min_len].pop()
+
+            added_new_word = False # Initialize the the flag for new additions
+
+            for word in wordList:
+
+                # if the word is already in our prefix, skip the word.
+                if word in prefix:
+                    continue
+
+                added_new_word = True # We have added at least one word
+
+                # Generate new possible proto_superstrings using the prefix and the wordList
+                for partition in generate_partitions(word):
+                    if prefix.endswith(partition.prefix):
+                        new_superstring = prefix + partition.sufix
+                        proto_superstrings[len(new_superstring)].add(new_superstring)
+
+            # If we reach the max_len then yield all the superstrings
+
+            if not added_new_word:
+                return prefix
+
+        # As we have used all proto_superstrings, delete the dict item for that
+        del proto_superstrings[min_len]
 
 def minSuperstringCheck(self,wordList, length):
     minSuper=minSuperstring(wordList)
@@ -39,7 +136,7 @@ class minSuperstringTest(unittest.TestCase):
         lLen=6
         l=list(map(chr, range(97, 97+lLen))) #["a","b",...]
         minSuperstringCheck(self,l,lLen)
-    
+
 
 if __name__ == "__main__":
     unittest.main()
